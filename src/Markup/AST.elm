0f9860b7d9f16b336788984b98949413ac786bea
James Carlson <jxxcarlson@gmail.com>
2021-10-04 11:55:44 -0400

    refine test function

--- src/Markup/AST.elm
+++ src/Markup/AST.elm
@@ -1,4 +1,14 @@
-module Markup.AST exposing (..)
+module Markup.AST exposing
+    ( BlockData
+    , BlockData2
+    , Expr(..)
+    , Loc
+    , Meta
+    , Position
+    , augment
+    , makeMeta
+    , test
+    )
 
 import List.Extra
 import Markup.Token as Token
@@ -17,6 +27,70 @@ type alias BlockData =
     }
 
 
+type alias BlockData2 =
+    { content : List String
+    , blockFirstLine : Int
+    , id : String
+    , index : List Int
+    , cumulativeLengths : List Int
+    }
+
+
+type alias Meta =
+    { id : String
+    , loc : Loc
+    }
+
+
+type alias Position =
+    { row : Int, col : Int }
+
+
+type alias Loc =
+    { begin : Position, end : Position }
+
+
+{-|
+
+    Construct a Meta value given
+        - the block count
+        - a location (tokenLoc: Token.Loc)
+        - blockData
+
+-}
+makeMeta : Int -> Token.Loc -> BlockData -> Meta
+makeMeta count tokenLoc blockData =
+    let
+        blockData2 =
+            augment blockData
+
+        n1 =
+            getLineNumber tokenLoc.begin blockData2.index
+
+        n2 =
+            getLineNumber tokenLoc.end blockData2.index
+
+        p1 =
+            List.take n1 blockData2.cumulativeLengths |> List.head |> Maybe.withDefault 0
+
+        p2 =
+            List.take n2 blockData2.cumulativeLengths |> List.head |> Maybe.withDefault 0
+
+        first =
+            { row = n1, col = tokenLoc.begin - p1 }
+
+        last =
+            { row = n2, col = tokenLoc.end - p2 }
+
+        loc =
+            { begin = first, end = last }
+    in
+    { id = blockData2.id ++ "." ++ String.fromInt count
+    , loc = loc
+    }
+
+
+test : Int -> Int -> String -> { accept : Bool, input : String, output : String, zmeta : Meta }
 test begin end content =
     let
         block =
@@ -27,17 +101,42 @@ test begin end content =
 
         tokenLoc =
             { begin = begin, end = end }
+
+        meta =
+            makeMeta 0 tokenLoc block
+
+        str =
+            stringAtLoc meta.loc block2.content
+
+        input =
+            String.slice begin (end + 1) content
     in
-    makeMeta 0 tokenLoc block2
+    { accept = input == str, input = input, output = str, zmeta = meta }
 
 
-type alias BlockData2 =
-    { content : List String
-    , blockFirstLine : Int
-    , id : String
-    , index : List Int
-    , cumulativeLengths : List Int
-    }
+stringAtLoc : Loc -> List String -> String
+stringAtLoc loc inputLines =
+    let
+        selectedLines =
+            List.filter (\( i, _ ) -> i >= loc.begin.row && i <= loc.end.row) (List.indexedMap (\i s -> ( i, s )) inputLines) |> Debug.log "SELECTED LINES"
+
+        n =
+            List.length selectedLines
+
+        transform ( i, line ) =
+            if i == loc.begin.row && i /= loc.end.row then
+                String.dropLeft loc.begin.col line
+
+            else if i == loc.begin.row && i == loc.end.row then
+                String.dropLeft loc.begin.col line |> String.dropRight (String.length line - loc.end.col - 1 |> Debug.log "INDEX FOR DROP RIGHT")
+
+            else if i == loc.end.row then
+                String.dropRight (String.length line - loc.end.col - 1 |> Debug.log "INDEX FOR DROP RIGHT") line
+
+            else
+                line
+    in
+    selectedLines |> List.map transform |> String.join "" |> Debug.log "stringAtLoc, OUT"
 
 
 augment : BlockData -> BlockData2
@@ -68,46 +167,3 @@ linePositions lines =
     List.foldl (\line acc -> (String.length line + head acc) :: acc) [ 0 ] lines
         |> List.drop 1
         |> List.reverse
-
-
-makeMeta : Int -> Token.Loc -> BlockData2 -> Meta
-makeMeta count tokenLoc blockData =
-    let
-        n1 =
-            getLineNumber tokenLoc.begin blockData.index
-
-        n2 =
-            getLineNumber tokenLoc.end blockData.index
-
-        p1 =
-            List.take n1 blockData.cumulativeLengths |> List.head |> Maybe.withDefault 0
-
-        p2 =
-            List.take n2 blockData.cumulativeLengths |> List.head |> Maybe.withDefault 0
-
-        first =
-            { row = n1, col = tokenLoc.begin - p1 }
-
-        last =
-            { row = n2, col = tokenLoc.end - p2 }
-
-        loc =
-            { begin = first, end = last }
-    in
-    { id = blockData.id ++ "." ++ String.fromInt count
-    , loc = loc
-    }
-
-
-type alias Meta =
-    { id : String
-    , loc : Loc
-    }
-
-
-type alias Position =
-    { row : Int, col : Int }
-
-
-type alias Loc =
-    { begin : Position, end : Position }
