be814594ab466e36afc2bf8c51d5370bac735331
James Carlson <jxxcarlson@gmail.com>
2021-10-06 13:13:31 -0400

    Add directory Block and module Block.Line in preparation for making a saner block parser; tests are now 38:0

--- tests/BlockTest.elm
+++ tests/BlockTest.elm
@@ -19,9 +19,8 @@ suiteBlock =
             \_ ->
                 blockTest MiniLaTeX "1.1" 0 "fee"
                     |> Expect.equal (Paragraph [ TextM "fee\n" { id = "1.1.0", loc = { begin = { col = 0, row = 0 }, end = { col = 3, row = 0 } } } ] { begin = 0, end = 1, id = "1.1", indent = 0 })
-        , Test.only <|
-            test "(2) BlockTest 1 6" <|
-                \_ ->
-                    blockTest MiniLaTeX "1.2" 0 "foo\nbar"
-                        |> Expect.equal (Paragraph [ TextM "foo\nbar\n" { id = "1.2.0", loc = { begin = { col = 0, row = 0 }, end = { col = 3, row = 1 } } } ] { begin = 0, end = 2, id = "1.2", indent = 0 })
+        , test "(2) BlockTest 1 6" <|
+            \_ ->
+                blockTest MiniLaTeX "1.2" 0 "foo\nbar"
+                    |> Expect.equal (Paragraph [ TextM "foo\nbar\n" { id = "1.2.0", loc = { begin = { col = 0, row = 0 }, end = { col = 3, row = 1 } } } ] { begin = 0, end = 2, id = "1.2", indent = 0 })
         ]
