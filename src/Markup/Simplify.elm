ce1e393408c80e17a62f1076fe392e37742966ca
James Carlson <jxxcarlson@gmail.com>
2021-10-11 04:23:24 -0400

    Write annotated text parser
    i Please enter the commit message for your changes. Lines starting

--- src/Markup/Simplify.elm
+++ src/Markup/Simplify.elm
@@ -26,6 +26,7 @@ type TokenS
     | SymbolST String
     | FunctionNameST String
     | MarkedTextST String String
+    | AnnotatedTextST String String
 
 
 stack : List (Either Token Expr) -> List (Either TokenS ExprS)
@@ -55,6 +56,9 @@ simplifyToken token =
 
         Token.MarkedText name str _ ->
             MarkedTextST name str
+
+        Token.AnnotatedText str1 str2 _ ->
+            AnnotatedTextST str1 str2
 
 
 blocks : List Block -> List BlockS
