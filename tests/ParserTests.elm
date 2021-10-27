1aa6eda8e7a9111a8ce72871a1ff814f5aa0ed30
James Carlson <jxxcarlson@gmail.com>
2021-10-27 19:20:44 -0400

    Update tests (52:0)

--- tests/ParserTests.elm
+++ tests/ParserTests.elm
@@ -5,6 +5,7 @@ import Expect
 import Expression.AST exposing (Expr(..))
 import Expression.Parser exposing (run)
 import Lang.Lang exposing (Lang(..))
+import Lang.Token.Common exposing (TokenState(..))
 import Markup.API as API
 import Markup.Simplify as Simplify exposing (BlockS(..), ExprS(..))
 import Test exposing (..)
@@ -29,15 +30,15 @@ suiteMarkdown =
         [ test "(1) foo" <|
             \_ ->
                 run Markdown "foo"
-                    |> Expect.equal { committed = [ Text "foo" { begin = 0, end = 2 } ], count = 2, end = 3, scanPointer = 3, sourceText = "foo", stack = [] }
+                    |> Expect.equal { committed = [ Text "foo" { begin = 0, end = 2 } ], count = 2, end = 3, scanPointer = 3, sourceText = "foo", stack = [], tokenState = TSA }
         , test "(2) BOLD" <|
             \_ ->
                 run Markdown "*foo*"
-                    |> Expect.equal { committed = [ Expr "strong" [ Text "foo" (loc 0 4) ] (loc 0 4) ], count = 2, end = 5, scanPointer = 5, sourceText = "*foo*", stack = [] }
+                    |> Expect.equal { committed = [ Expr "strong" [ Text "foo" (loc 0 4) ] (loc 0 4) ], count = 2, end = 5, scanPointer = 5, sourceText = "*foo*", stack = [], tokenState = TSA }
         , test "(3) LINK" <|
             \_ ->
                 run Markdown "[N Y T](url)"
-                    |> Expect.equal { committed = [Expr "link" [Text "N Y T" { begin = 1, end = 5 },Text "url" { begin = 8, end = 10 }] { begin = 0, end = 11 }], count = 7, end = 12, scanPointer = 12, sourceText = "[N Y T](url)", stack = [] }
+                    |> Expect.equal { committed = [ Expr "link" [ Text "N Y T" { begin = 1, end = 5 }, Text "url" { begin = 8, end = 10 } ] { begin = 0, end = 11 } ], count = 7, end = 12, scanPointer = 12, sourceText = "[N Y T](url)", stack = [], tokenState = TSA }
         ]
 
 
@@ -47,7 +48,7 @@ suiteMiniLaTeX =
         [ test "(1) foo" <|
             \_ ->
                 run MiniLaTeX "foo"
-                    |> Expect.equal { committed = [ Text "foo" (loc 0 2) ], count = 2, end = 3, scanPointer = 3, sourceText = "foo", stack = [] }
+                    |> Expect.equal { committed = [ Text "foo" (loc 0 2) ], count = 2, end = 3, scanPointer = 3, sourceText = "foo", stack = [], tokenState = TSA }
         , test "(2) \\foo" <|
             \_ ->
                 run MiniLaTeX "\\foo"
@@ -57,31 +58,31 @@ suiteMiniLaTeX =
         , test "(3) \\foo{1}" <|
             \_ ->
                 run MiniLaTeX "\\foo{1}"
-                    |> Expect.equal { committed = [ Expr "foo" [ Text "1" (loc 5 5) ] (loc 0 6) ], count = 5, end = 7, scanPointer = 7, sourceText = "\\foo{1}", stack = [] }
+                    |> Expect.equal { committed = [ Expr "foo" [ Text "1" (loc 5 5) ] (loc 0 6) ], count = 5, end = 7, scanPointer = 7, sourceText = "\\foo{1}", stack = [], tokenState = TSA }
         , test "(4) \\foo{1}{2}" <|
             \_ ->
                 run MiniLaTeX "\\foo{1}{2}"
-                    |> Expect.equal { committed = [ Expr "foo" [ Text "1" (loc 5 5), Text "2" (loc 8 8) ] (loc 0 9) ], count = 8, end = 10, scanPointer = 10, sourceText = "\\foo{1}{2}", stack = [] }
+                    |> Expect.equal { committed = [ Expr "foo" [ Text "1" (loc 5 5), Text "2" (loc 8 8) ] (loc 0 9) ], count = 8, end = 10, scanPointer = 10, sourceText = "\\foo{1}{2}", stack = [], tokenState = TSA }
         , test "(5) abc \\foo{1} def" <|
             \_ ->
                 run MiniLaTeX "abc \\foo{1} def"
-                    |> Expect.equal { committed = [ Text "abc " (loc 0 3), Expr "foo" [ Text "1" (loc 9 9) ] (loc 4 10), Text " def" (loc 11 14) ], count = 7, end = 15, scanPointer = 15, sourceText = "abc \\foo{1} def", stack = [] }
+                    |> Expect.equal { committed = [ Text "abc " (loc 0 3), Expr "foo" [ Text "1" (loc 9 9) ] (loc 4 10), Text " def" (loc 11 14) ], count = 7, end = 15, scanPointer = 15, sourceText = "abc \\foo{1} def", stack = [], tokenState = TSA }
         , test "(6) \\foo{\\bar{1}}" <|
             \_ ->
                 run MiniLaTeX "\\foo{\\bar{1}}"
-                    |> Expect.equal { committed = [ Expr "foo" [ Expr "bar" [ Text "1" (loc 10 10) ] (loc 5 11) ] (loc 0 12) ], count = 8, end = 13, scanPointer = 13, sourceText = "\\foo{\\bar{1}}", stack = [] }
+                    |> Expect.equal { committed = [ Expr "foo" [ Expr "bar" [ Text "1" (loc 10 10) ] (loc 5 11) ] (loc 0 12) ], count = 8, end = 13, scanPointer = 13, sourceText = "\\foo{\\bar{1}}", stack = [], tokenState = TSA }
         , test "(7) \\foo{\\bar{1}}" <|
             \_ ->
                 run MiniLaTeX "$x^2$"
-                    |> Expect.equal { committed = [ Verbatim "math" "x^2" (loc 0 4) ], count = 2, end = 5, scanPointer = 5, sourceText = "$x^2$", stack = [] }
+                    |> Expect.equal { committed = [ Verbatim "math" "x^2" (loc 0 4) ], count = 2, end = 5, scanPointer = 5, sourceText = "$x^2$", stack = [], tokenState = TSA }
         , test "(8)  abc \\href{1}{2} def" <|
             \_ ->
                 run MiniLaTeX "\\href{1}{2}"
-                    |> Expect.equal { committed = [ Expr "href" [ Text "1" { begin = 6, end = 6 }, Text "2" { begin = 9, end = 9 } ] { begin = 0, end = 10 } ], count = 8, end = 11, scanPointer = 11, sourceText = "\\href{1}{2}", stack = [] }
+                    |> Expect.equal { committed = [ Expr "href" [ Text "1" { begin = 6, end = 6 }, Text "2" { begin = 9, end = 9 } ] { begin = 0, end = 10 } ], count = 8, end = 11, scanPointer = 11, sourceText = "\\href{1}{2}", stack = [], tokenState = TSA }
         , test "(9) abc \\href{1}{2} def" <|
             \_ ->
                 run MiniLaTeX "abc \\href{1}{2} def"
-                    |> Expect.equal { committed = [Text "abc " { begin = 0, end = 3 },Expr "href" [Text "1" { begin = 10, end = 10 },Text "2" { begin = 13, end = 13 }] { begin = 4, end = 14 },Text " def" { begin = 15, end = 18 }], count = 10, end = 19, scanPointer = 19, sourceText = "abc \\href{1}{2} def", stack = [] }
+                    |> Expect.equal { committed = [ Text "abc " { begin = 0, end = 3 }, Expr "href" [ Text "1" { begin = 10, end = 10 }, Text "2" { begin = 13, end = 13 } ] { begin = 4, end = 14 }, Text " def" { begin = 15, end = 18 } ], count = 10, end = 19, scanPointer = 19, sourceText = "abc \\href{1}{2} def", stack = [], tokenState = TSA }
         ]
 
 
@@ -91,7 +92,7 @@ suiteL1 =
         [ test "(1) foo" <|
             \_ ->
                 run L1 "foo"
-                    |> Expect.equal { committed = [ Text "foo" (loc 0 2) ], count = 2, end = 3, scanPointer = 3, sourceText = "foo", stack = [] }
+                    |> Expect.equal { committed = [ Text "foo" (loc 0 2) ], count = 2, end = 3, scanPointer = 3, sourceText = "foo", stack = [], tokenState = TSA }
         , test "(2) foo [i ABC]" <|
             \_ ->
                 p L1 "foo [i ABC]"
